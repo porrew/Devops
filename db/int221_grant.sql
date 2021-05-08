d62c75f01e6e8db726988cff2a94bc7690ba5d4b
Korawit Rittisit <korawit.oum@mail.kmutt.ac.th>
2021-05-09 00:50:10 +0700

    fix code

--- db/int221_grant.sql
+++ db/int221_grant.sql
@@ -1,2 +1,2 @@
-create user 'backend'@'172.27.0.3' identified by 'Password@Project221';
+create user 'backend'@'172.25.0.3' identified by 'Password@Project221';
 grant select,insert,update,delete on mydb.* to 'backend'@'172.25.0.3';
